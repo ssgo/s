77ba4a6dd88cdaaf9aa7a6ea7fc8457e8c2a23ed
Star <star@Stars-Mac.local>
2017-12-19 19:44:16 +0800

    基本支持数据库各种操作

--- DB.go
+++ DB.go
@@ -5,30 +5,357 @@ import (
 	"database/sql"
 	"fmt"
 	"reflect"
-	"encoding/base64"
-	"crypto/aes"
-	"crypto/cipher"
+	"time"
+	"strings"
 )
 
 type dbInfo struct {
-	Type     string
-	User     string
-	Password string
-	Host     string
-	DB       string
+	Type        string
+	User        string
+	Password    string
+	Host        string
+	DB          string
+	MaxOpens    int
+	MaxIdles    int
+	MaxLifeTime time.Duration
 }
 
 type DB struct {
 	conn *sql.DB
 }
 
-var key, iv []byte
-func SetEncryptKeys(key1, key2 []byte){
-	key = key1
-	iv = key2
+type Tx struct {
+	conn *sql.Tx
+}
+
+type Stmt struct {
+	conn *sql.Stmt
+}
+
+var settedKey = []byte("vpL54DlR2KG{JSAaAX7Tu;*#&DnG`M0o")
+var settedIv = []byte("@z]zv@10-K.5Al0Dm`@foq9k\"VRfJ^~j")
+var keysSetted = false
+
+func SetEncryptKeys(key, iv []byte) {
+	if !keysSetted {
+		settedKey = key
+		settedIv = iv
+		keysSetted = true
+	}
+}
+
+var dbConfigs = make(map[string]dbInfo)
+var dbInstances = make(map[string]*DB)
+
+func GetDB(name string) (*DB, error) {
+	if dbInstances[name] != nil {
+		return dbInstances[name], nil
+	}
+
+	if len(dbConfigs) == 0 {
+		base.LoadConfig("db", &dbConfigs)
+	}
+
+	conf := dbConfigs[name]
+	if conf.Host == "" {
+		return nil, fmt.Errorf("No db seted for %s", name)
+	}
+	if conf.Type == "" {
+		conf.Type = "mysql"
+	}
+
+	connectType := "tcp"
+	if []byte(conf.Host)[0] == '/' {
+		connectType = "unix"
+	}
+
+	conn, err := sql.Open(conf.Type, fmt.Sprintf("%s:%s@%s(%s)/%s", conf.User, base.DecryptAes(conf.Password, settedKey, settedIv), connectType, conf.Host, conf.DB))
+	if err != nil {
+		return nil, err
+	}
+	db := new(DB)
+	db.conn = conn
+	if conf.MaxIdles > 0 {
+		conn.SetMaxIdleConns(conf.MaxIdles)
+	}
+	if conf.MaxOpens > 0 {
+		conn.SetMaxOpenConns(conf.MaxOpens)
+	}
+	if conf.MaxLifeTime > 0 {
+		conn.SetConnMaxLifetime(conf.MaxLifeTime)
+	}
+	dbInstances[name] = db
+	return db, err
+}
+
+func (this *DB) Destroy() error {
+	return this.conn.Close()
+}
+
+func (this *DB) GetConnection() *sql.DB {
+	return this.conn
+}
+
+func (this *Tx) Commit() error {
+	return this.conn.Commit()
+}
+
+func (this *Tx) Rollback() error {
+	return this.conn.Rollback()
+}
+
+func (this *Stmt) Exec(args ...interface{}) (int64, error) {
+	r, err := this.conn.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	numChanges, err := r.RowsAffected()
+	if err != nil {
+		return 0, nil
+	}
+	return numChanges, nil
+}
+
+func (this *Stmt) Close() error {
+	return this.conn.Close()
+}
+
+func (this *DB) Prepare(requestSql string) (*Stmt, error) {
+	return basePrepare(this.conn, nil, requestSql)
+}
+func (this *Tx) Prepare(requestSql string) (*Stmt, error) {
+	return basePrepare(nil, this.conn, requestSql)
+}
+func basePrepare(db *sql.DB, tx *sql.Tx, requestSql string) (*Stmt, error) {
+	var sqlStmt *sql.Stmt
+	var err error
+	if tx != nil {
+		sqlStmt, err = tx.Prepare(requestSql)
+	}else{
+		sqlStmt, err = db.Prepare(requestSql)
+	}
+	if err != nil {
+		return nil, err
+	}
+	stmt := new(Stmt)
+	stmt.conn = sqlStmt
+	return stmt, nil
+}
+
+func (this *DB) Begin() (*Tx, error) {
+	sqlTx, err := this.conn.Begin()
+	if err != nil {
+		return nil, err
+	}
+	tx := new(Tx)
+	tx.conn = sqlTx
+	return tx, nil
+}
+
+func (this *DB) Exec(requestSql string, args ...interface{}) (int64, error) {
+	return baseExec(this.conn, nil, requestSql, args...)
+}
+func (this *Tx) Exec(requestSql string, args ...interface{}) (int64, error) {
+	return baseExec(nil, this.conn, requestSql, args...)
+}
+func baseExec(db *sql.DB, tx *sql.Tx, requestSql string, args ...interface{}) (int64, error) {
+	var r sql.Result
+	var err error
+	if tx != nil {
+		r, err = tx.Exec(requestSql, args...)
+	}else{
+		r, err = db.Exec(requestSql, args...)
+	}
+
+	if err != nil {
+		return 0, err
+	}
+	numChanges, err := r.RowsAffected()
+	if err != nil {
+		return 0, nil
+	}
+	return numChanges, nil
+}
+
+func (this *DB) ExecInsert(requestSql string, args ...interface{}) (int64, error) {
+	return baseExecInsert(this.conn, nil, requestSql, args...)
+}
+func (this *Tx) ExecInsert(requestSql string, args ...interface{}) (int64, error) {
+	return baseExecInsert(nil, this.conn, requestSql, args...)
+}
+func baseExecInsert(db *sql.DB, tx *sql.Tx, requestSql string, args ...interface{}) (int64, error) {
+	var r sql.Result
+	var err error
+	if tx != nil {
+		r, err = tx.Exec(requestSql, args...)
+	}else{
+		r, err = db.Exec(requestSql, args...)
+	}
+
+	if err != nil {
+		return 0, err
+	}
+	insertId, err := r.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+	return insertId, nil
+}
+func (this *Stmt) ExecInsert(args ...interface{}) (int64, error) {
+	r, err := this.conn.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	insertId, err := r.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+	return insertId, nil
 }
 
 func (this *DB) Query(results interface{}, requestSql string, args ...interface{}) error {
+	return baseQuery(this.conn, nil, results, requestSql, args...)
+}
+func (this *Tx) Query(results interface{}, requestSql string, args ...interface{}) error {
+	return baseQuery(nil, this.conn, results, requestSql, args...)
+}
+func baseQuery(db *sql.DB, tx *sql.Tx, results interface{}, requestSql string, args ...interface{}) error {
+	var rows *sql.Rows
+	var err error
+	if tx != nil {
+		rows, err = tx.Query(requestSql, args...)
+	}else{
+		rows, err = db.Query(requestSql, args...)
+	}
+	if err != nil {
+		return err
+	}
+	return makeResults(results, rows)
+}
+
+func (this *DB) Insert(table string, data interface{}) (int64, error) {
+	return baseInsert(this.conn, nil, table, data, false)
+}
+func (this *Tx) Insert(table string, data interface{}) (int64, error) {
+	return baseInsert(nil, this.conn, table, data, false)
+}
+func (this *DB) Replace(table string, data interface{}) (int64, error) {
+	return baseInsert(this.conn, nil, table, data, true)
+}
+func (this *Tx) Replace(table string, data interface{}) (int64, error) {
+	return baseInsert(nil, this.conn, table, data, true)
+}
+func baseInsert(db *sql.DB, tx *sql.Tx, table string, data interface{}, useReplace bool) (int64, error) {
+	keys, vars, values := makeKeysVarsValues(data)
+	var operation string
+	if useReplace {
+		operation = "replace"
+	} else {
+		operation = "insert"
+	}
+	requestSql := fmt.Sprintf("%s into `%s` (`%s`) values (%s)", operation, table, strings.Join(keys, "`,`"), strings.Join(vars, ","))
+
+	var result sql.Result
+	var err error
+	if tx != nil {
+		result, err = tx.Exec(requestSql, values...)
+	}else{
+		result, err = db.Exec(requestSql, values...)
+	}
+	if err != nil {
+		return 0, err
+	}
+	lastInsertId, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+	return lastInsertId, nil
+}
+
+func (this *DB) Update(table string, data interface{}, wheres string, args ...interface{}) (int64, error) {
+	return baseUpdate(this.conn, nil, table, data, wheres, args...)
+}
+func (this *Tx) Update(table string, data interface{}, wheres string, args ...interface{}) (int64, error) {
+	return baseUpdate(nil, this.conn, table, data, wheres, args...)
+}
+func baseUpdate(db *sql.DB, tx *sql.Tx, table string, data interface{}, wheres string, args ...interface{}) (int64, error) {
+	keys, vars, values := makeKeysVarsValues(data)
+	for i, k := range keys {
+		keys[i] = fmt.Sprintf( "`%s`=%s", k, vars[i] )
+	}
+	for _, v := range args {
+		values = append(values, v)
+	}
+	requestSql := fmt.Sprintf("update `%s` set %s where %s", table, strings.Join(keys, ","), wheres)
+
+	var result sql.Result
+	var err error
+	if tx != nil {
+		result, err = tx.Exec(requestSql, values...)
+	}else{
+		result, err = db.Exec(requestSql, values...)
+	}
+	if err != nil {
+		return 0, err
+	}
+	if err != nil {
+		return 0, err
+	}
+	numChanges, err := result.RowsAffected()
+	if err != nil {
+		return 0, nil
+	}
+	return numChanges, nil
+}
+
+func makeKeysVarsValues(data interface{}) ([]string, []string, []interface{}) {
+	keys := make([]string, 0)
+	vars := make([]string, 0)
+	values := make([]interface{}, 0)
+
+	dataType := reflect.TypeOf(data)
+	dataValue := reflect.ValueOf(data)
+	if dataType.Kind() == reflect.Ptr {
+		dataType = dataType.Elem()
+		dataValue = dataValue.Elem()
+	}
+
+	if dataType.Kind() == reflect.Struct {
+		// 按结构处理数据
+		for i := 0; i < dataType.NumField(); i++ {
+			v := dataValue.Field(i)
+			if v.Kind() == reflect.Interface {
+				v = v.Elem()
+			}
+			keys = append(keys, dataType.Field(i).Name)
+			if v.Kind() == reflect.String && []byte(v.String())[0] == ':' {
+				vars = append(vars, string([]byte(v.String())[1:]))
+			} else {
+				vars = append(vars, "?")
+				values = append(values, v.Interface())
+			}
+		}
+	} else if dataType.Kind() == reflect.Map {
+		// 按Map处理数据
+		for _, k := range dataValue.MapKeys() {
+			v := dataValue.MapIndex(k)
+			if v.Kind() == reflect.Interface {
+				v = v.Elem()
+			}
+			keys = append(keys, k.String())
+			if v.Kind() == reflect.String && []byte(v.String())[0] == ':' {
+				vars = append(vars, string([]byte(v.String())[1:]))
+			} else {
+				vars = append(vars, "?")
+				values = append(values, v.Interface())
+			}
+		}
+	}
+
+	return keys, vars, values
+}
+
+func makeResults(results interface{}, rows *sql.Rows) error {
 	rowType := reflect.TypeOf(results)
 	resultsValue := reflect.ValueOf(results)
 	if rowType.Kind() != reflect.Ptr {
@@ -37,14 +364,7 @@ func (this *DB) Query(results interface{}, requestSql string, args ...interface{
 	rowType = rowType.Elem()
 	resultsValue = resultsValue.Elem()
 
-	var rows *sql.Rows
-	var colTypes []*sql.ColumnType
-	var err error
-
-	rows, err = this.conn.Query(requestSql, args...)
-	if err == nil {
-		colTypes, err = rows.ColumnTypes()
-	}
+	colTypes, err := rows.ColumnTypes()
 	if err != nil {
 		return err
 	}
@@ -55,44 +375,48 @@ func (this *DB) Query(results interface{}, requestSql string, args ...interface{
 		rowType = rowType.Elem()
 	}
 
-	scanValues := make([]interface{},colNum)
+	scanValues := make([]interface{}, colNum)
 	if rowType.Kind() == reflect.Struct {
 		// 按结构处理数据
 		for colIndex, col := range colTypes {
 			publicColName := makePublicVarName(col.Name())
 			field, found := rowType.FieldByName(publicColName)
 			if found {
-				scanValues[colIndex] = makeValue(field.Type)
-			}else{
+				if field.Type.Kind() == reflect.Interface {
+					scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
+				} else {
+					scanValues[colIndex] = makeValue(field.Type)
+				}
+			} else {
 				scanValues[colIndex] = new(string)
 			}
 		}
 	} else if rowType.Kind() == reflect.Map {
 		// 按Map处理数据
 		for colIndex := range colTypes {
-			if rowType.Elem().Kind() == reflect.Interface{
+			if rowType.Elem().Kind() == reflect.Interface {
 				scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
-			}else{
+			} else {
 				scanValues[colIndex] = makeValue(rowType.Elem())
 			}
 		}
 	} else if rowType.Kind() == reflect.Slice {
 		// 按Map处理数据
 		for colIndex := range colTypes {
-			if rowType.Elem().Kind() == reflect.Interface{
+			if rowType.Elem().Kind() == reflect.Interface {
 				scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
-			}else{
+			} else {
 				scanValues[colIndex] = makeValue(rowType.Elem())
 			}
 		}
 	} else {
 		// 只返回一列结果
-		if rowType.Kind() == reflect.Interface{
+		if rowType.Kind() == reflect.Interface {
 			scanValues[0] = makeValue(colTypes[0].ScanType())
-		}else{
+		} else {
 			scanValues[0] = makeValue(rowType)
 		}
-		for colIndex:=1; colIndex<colNum; colIndex++ {
+		for colIndex := 1; colIndex < colNum; colIndex++ {
 			scanValues[colIndex] = new(string)
 		}
 	}
@@ -133,29 +457,31 @@ func (this *DB) Query(results interface{}, requestSql string, args ...interface{
 
 		if resultsValue.Kind() == reflect.Slice {
 			resultsValue = reflect.Append(resultsValue, data)
-		}else{
+		} else {
 			resultsValue = data
 			break
 		}
 	}
 
 	reflect.ValueOf(results).Elem().Set(resultsValue)
-
+	//if this.conn.Stats().OpenConnections > 1 {
+	//	fmt.Println("conns:", this.conn.Stats().OpenConnections)
+	//}
 	return nil
 }
 
-func makePublicVarName(name string) string{
+func makePublicVarName(name string) string {
 	colNameBytes := []byte(name)
 	if colNameBytes[0] >= 97 {
 		colNameBytes[0] -= 32
 		return string(colNameBytes)
-	}else{
+	} else {
 		return name
 	}
 }
 
-func makeValue(t reflect.Type) interface{}{
-	if t.Kind() == reflect.Ptr{
+func makeValue(t reflect.Type) interface{} {
+	if t.Kind() == reflect.Ptr {
 		t = t.Elem()
 	}
 
@@ -189,50 +515,9 @@ func makeValue(t reflect.Type) interface{}{
 	case reflect.String:
 		return new(string)
 	}
+
 	return new(string)
-}
-
-func (db *DB) Close() error {
-	return db.conn.Close()
-}
-
-var dbConfigs = make(map[string]dbInfo)
-
-func GetDB(name string) (*DB, error) {
-	if len(dbConfigs) == 0 {
-		base.LoadConfig("db", &dbConfigs)
-	}
-
-	conf := dbConfigs[name]
-	if conf.Host == "" {
-		return nil, fmt.Errorf("No db seted for %s", name)
-	}
-	if conf.Type == "" {
-		conf.Type = "mysql"
-	}
-
-	conn, err := sql.Open(conf.Type, fmt.Sprintf("%s:%s@tcp(%s)/%s", conf.User, aesDecrypt(conf.Password, key, iv), conf.Host, conf.DB))
-	db := new(DB)
-	db.conn = conn
-	return db, err
-}
-
-func aesDecrypt(crypted string, key []byte, iv []byte) string {
-	cryptedBytes, err := base64.StdEncoding.DecodeString(crypted)
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return ""
-	}
-	blockSize := block.BlockSize()
-	blockMode := cipher.NewCBCDecrypter(block, iv[:blockSize])
-	origData := make([]byte, len(cryptedBytes))
-	blockMode.CryptBlocks(origData, cryptedBytes)
-	origData = pkcs5UnPadding(origData)
-	return string(origData)
-}
-
-func pkcs5UnPadding(origData []byte) []byte {
-	length := len(origData)
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
+	//if t.Kind() == reflect.Slice && t.Elem().Kind() == reflect.Uint8{
+	//	return new(string)
+	//}
 }
--- README.md
+++ README.md
@@ -1,26 +1,113 @@
 # Go语言的一个数据库操作封装
 核心思想是传入一个结果容器，根据容器的类型自动填充数据，方便使用
 
-## passwordMaker
+## 配置
 
-把 passwordMaker/PasswordMaker.sample 复制为 passwordMaker/PasswordMaker.go
+```json
+{
+  "test": {
+    "type": "mysql",
+    "user": "test",
+    "password": "34RVCy0rQBSQmLX64xjoyg==",	// 使用 github.com/ssgo/base/passwordMaker 生成
+    "host": "/tmp/mysql.sock",
+    "db": "test",
+    "maxOpens": 100,	// 最大连接数，0表示不限制
+    "maxIdles": 30,		// 最大空闲连接，0表示不限制
+    "maxLiftTime": 0	// 每个连接的存活时间，0表示永远
+  }
+}
+```
 
-修改其中的 key 和 iv 的内容，长度至少32字节
-
-执行 go run passwordMaker/*.go '你的密码' 生成加密后的密码
 
 
-## tests
+## API
 
-### 把 tests/dbInit.go.sample 复制为 tests/dbInit.go
+```go
+import github.com/ssgo/db
 
-修改其中的 key 和 iv 的内容，长度至少32字节，保持与passwordMaker 中一致
+// 自定义加密密钥，
+func SetEncryptKeys(key, iv []byte){}
 
-### 把 tests/db.json.sample 复制为 tests/db.json
+// 获得一个数据库操作实例，这是一个连接池，直接操作即可不需要实例化
+func GetDB(name string) (*DB, error){}
+
+
+// 释放数据库操作实例，正常情况下不应该操作，否则整个连接池都将无法使用
+func (this *DB) Destroy() error{}
+
+// 取得原始的 sql.DB 对象，可自行操作
+func (this *DB) GetConnection() *sql.DB{}
+
+// 查询数据，根据接收结果的对象不同执行不同的查询
+// results := make([]userInfo, 0)               取全部结果，存为对象
+// results := make([]map[string]interface{}, 0) 取全部结果，保留原始类型
+// results := make([]map[string]string, 0)      取全部结果，统一转换为string
+// results := make([]map[string]int, 0)         取全部结果，统一转换为int，不可转换会报错
+// results := make([][]string, 0)               取全部结果，存为数组
+// results := userInfo{}                        取第一行数据，存为对象
+// results := map[string]interface{}{}          取第一行数据，存为map
+// results := make([]string, 0)                 取全部第一列数据
+// var results int                              取第一行第一列数据
+func (this *DB) Query(results interface{}, requestSql string, args ...interface{}) error {}
+
+// 执行普通查询，返回影响列数
+func (this *DB) Exec(requestSql string, args ...interface{}) (int64, error) {}
+
+// 执行普通INSERT或REPLACE，返回lastInsertId
+func (this *DB) ExecInsert(requestSql string, args ...interface{}) (int64, error) {}
+
+// 按数据对象自动生成INSERT语句并执行，data支持Map和Struct
+func (this *DB) Insert(table string, data interface{}) (int64, error) {}
+
+// 按数据对象自动生成REPLACE语句并执行，data支持Map和Struct
+func (this *DB) Replace(table string, data interface{}) (int64, error) {}
+
+// 按数据对象自动生成UPDATE语句并执行，data支持Map和Struct
+func (this *DB) Update(table string, data interface{}, wheres string, args ...interface{}) (int64, error) {}
+
+// 开启一个事务
+func (this *DB) Begin() (*Tx, error) {}
+
+// 预处理
+func (this *DB) Prepare(requestSql string) (*Stmt, error) {}
+
+
+// 在事务中操作，同(this *DB)
+func (this *Tx) Query(results interface{}, requestSql string, args ...interface{}) error {}
+func (this *Tx) Exec(requestSql string, args ...interface{}) (int64, error) {}
+func (this *Tx) ExecInsert(requestSql string, args ...interface{}) (int64, error) {}
+func (this *Tx) Insert(table string, data interface{}) (int64, error) {}
+func (this *Tx) Replace(table string, data interface{}) (int64, error) {}
+func (this *Tx) Update(table string, data interface{}, wheres string, args ...interface{}) (int64, error) {}
+func (this *Tx) Prepare(requestSql string) (*Stmt, error) {
+
+// 提交
+func (this *Tx) Commit() error {
+
+// 回滚
+func (this *Tx) Rollback() error {
+
+
+// 批量执行，返回受影响的列数
+func (this *Stmt) Exec(args ...interface{}) (int64, error) {}
+
+// 批量执行，返回lastInsertId
+func (this *Stmt) ExecInsert(args ...interface{}) (int64, error) {}
+
+// 关闭预处理
+func (this *Stmt) Close() error {}
+
+```
+
+
+
+## 运行测试用例
+
+#### 把 tests/db.json.sample 复制为 tests/db.json
 
 修改你本地数据库连接信息
 
-使用 passwordMaker 创建加密后的密码写入 db.json 中的 password
+使用 github.com/ssgo/base 中的 passwordMaker 创建加密后的密码写入 db.json 中的 password
 
 进入 tests 目录运行 go test
 
@@ -28,14 +115,18 @@
 
 ## 项目中的配置
 
-### 把 /dbInit.go.sample 复制到你项目中命名为 /dbInit.go
+#### 把 /db.json.sample 复制到你项目中命名为 /db.json
+
+修改你本地数据库连接信息
+
+使用 passwordMaker 创建加密后的密码写入 db.json 中的 password
+
+### 自定义加密（强烈推荐）
+
+##### 把 /dbInit.go.sample 复制到你项目中命名为 /dbInit.go
 
 修改其中的 key 和 iv 的内容，长度至少32字节，保持与passwordMaker 中一致
 
 也可以以其他方式只要在 init 函数中调用 db.SetEncryptKeys 设置匹配的 key和iv即可
 
-### 把 /db.json.sample 复制到你项目中命名为 /db.json
-
-修改你本地数据库连接信息
-
-使用 passwordMaker 创建加密后的密码写入 db.json 中的 password
+### 
