de662007e414be86c0201da4b7c38f9639c3707b
Star <star@Stars-Mac.local>
2018-01-05 21:57:28 +0800

    增加类型转换处理

--- Cast.go
+++ Cast.go
@@ -5,47 +5,202 @@ import (
 	"fmt"
 )
 
-func ToInt(value interface{}) int64 {
+func Int(value interface{}) int {
+	return int(Int64(value))
+}
+func Int64(value interface{}) int64 {
 	switch realValue := value.(type) {
 	case int:
+		return int64(realValue)
 	case int8:
+		return int64(realValue)
 	case int16:
+		return int64(realValue)
 	case int32:
+		return int64(realValue)
 	case int64:
+		return int64(realValue)
 	case float32:
+		return int64(realValue)
 	case float64:
 		return int64(realValue)
 	case bool:
-		if realValue{
+		if realValue {
 			return 1
-		}else {
+		} else {
 			return 0
 		}
+	case []byte:
+		i, err := strconv.ParseInt(string(realValue), 10, 0)
+		if err == nil {
+			return i
+		}
 	case string:
-		i, err := strconv.Atoi(realValue)
-		if err ==nil {
-			return int64(i)
+		i, err := strconv.ParseInt(realValue, 10, 0)
+		if err == nil {
+			return i
 		}
 	}
 	return 0
 }
 
-func ToString(value interface{}) string {
+func Float(value interface{}) float32 {
+	return float32(Float64(value))
+}
+func Float64(value interface{}) float64 {
 	switch realValue := value.(type) {
 	case int:
+		return float64(realValue)
 	case int8:
+		return float64(realValue)
 	case int16:
+		return float64(realValue)
 	case int32:
+		return float64(realValue)
 	case int64:
-		return strconv.Itoa(int(realValue))
+		return float64(realValue)
+	case float32:
+		return float64(realValue)
+	case float64:
+		return float64(realValue)
 	case bool:
-		if realValue{
+		if realValue {
+			return 1
+		} else {
+			return 0
+		}
+	case []byte:
+		i, err := strconv.ParseFloat(string(realValue), 10)
+		if err == nil {
+			return i
+		}
+	case string:
+		i, err := strconv.ParseFloat(realValue, 10)
+		if err == nil {
+			return i
+		}
+	}
+	return 0
+}
+
+func Bytes(value interface{}) []byte {
+	return []byte(String(value))
+}
+func String(value interface{}) string {
+	switch realValue := value.(type) {
+	case int:
+		return strconv.FormatInt(int64(realValue), 10)
+	case int8:
+		return strconv.FormatInt(int64(realValue), 10)
+	case int16:
+		return strconv.FormatInt(int64(realValue), 10)
+	case int32:
+		return strconv.FormatInt(int64(realValue), 10)
+	case int64:
+		return strconv.FormatInt(realValue, 10)
+	case bool:
+		if realValue {
 			return "true"
-		}else {
+		} else {
 			return "false"
 		}
 	case string:
 		return realValue
+	case []byte:
+		return string(realValue)
 	}
 	return fmt.Sprint(value)
+}
+
+func Bool(value interface{}) bool {
+	switch realValue := value.(type) {
+	case int, int8, int16, int32, int64, float32, float64:
+		return realValue != 0
+	case bool:
+		return realValue
+	case []byte:
+		switch string(realValue) {
+		case "1", "t", "T", "true", "TRUE", "True":
+			return true
+		}
+	case string:
+		switch realValue {
+		case "1", "t", "T", "true", "TRUE", "True":
+			return true
+		}
+	}
+	return false
+}
+
+func Ints(value interface{}) []int64 {
+	switch realValue := value.(type) {
+	case []interface{}:
+		result := make([]int64, len(realValue))
+		for i, v := range realValue {
+			result[i] = Int64(v)
+		}
+		return result
+	default:
+		return []int64{Int64(value)}
+	}
+	return make([]int64, 0)
+}
+
+func Floats(value interface{}) []float64 {
+	switch realValue := value.(type) {
+	case []interface{}:
+		result := make([]float64, len(realValue))
+		for i, v := range realValue {
+			result[i] = Float64(v)
+		}
+		return result
+	default:
+		return []float64{Float64(value)}
+	}
+	return make([]float64, 0)
+}
+
+func Strings(value interface{}) []string {
+	switch realValue := value.(type) {
+	case []interface{}:
+		result := make([]string, len(realValue))
+		for i, v := range realValue {
+			result[i] = String(v)
+		}
+		return result
+	default:
+		return []string{String(value)}
+	}
+	return make([]string, 0)
+}
+
+func FixUpperCase(data []byte){
+	n := len(data)
+	types := make([]bool, 0)
+	tpos := -1
+
+	for i := 0; i < n-1; i++ {
+		if tpos+1 >= len(types) {
+			types = append(types, false)
+		}
+		if data[i] == '{' {
+			tpos ++
+			types[tpos] = true
+			//log.Println(" >>>1 ", types, tpos)
+		} else if data[i] == '}' {
+			tpos --
+			//log.Println(" >>>2 ", types, tpos)
+		}
+		if data[i] == '[' {
+			tpos ++
+			types[tpos] = false
+			//log.Println(" >>>3 ", types, tpos)
+		} else if data[i] == ']' {
+			tpos --
+			//log.Println(" >>>4 ", types, tpos)
+		}
+		if data[i] == '"' && (data[i-1] == '{' || (data[i-1] == ',' && tpos >= 0 && types[tpos])) && (data[i+1] >= 'A' && data[i+1] <= 'Z') {
+			data[i+1] += 32
+		}
+	}
 }
