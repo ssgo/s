2b1455745346f0fbb0e6aaf797baa54892a2a20b
Star <star@Stars-Mac.local>
2018-02-07 19:17:10 +0800

    split files update error logs add sql info to error logs

--- DB.go
+++ DB.go
@@ -1,14 +1,10 @@
 package db
 
-// TODO 字段值为NULL时的处理
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/ssgo/base"
-	"log"
-	"reflect"
-	"runtime"
-	"strings"
 	"time"
 )
 
@@ -26,26 +22,6 @@ type dbInfo struct {
 type DB struct {
 	conn  *sql.DB
 	Error error
-}
-
-type Tx struct {
-	conn  *sql.Tx
-	Error error
-}
-
-type Stmt struct {
-	conn  *sql.Stmt
-	Error error
-}
-
-type QueryResult struct {
-	rows  *sql.Rows
-	Error error
-}
-
-type ExecResult struct {
-	result sql.Result
-	Error  error
 }
 
 var settedKey = []byte("vpL54DlR2KG{JSAaAX7Tu;*#&DnG`M0o")
@@ -102,7 +78,8 @@ func GetDB(name string) *DB {
 
 	conn, err := sql.Open(conf.Type, fmt.Sprintf("%s:%s@%s(%s)/%s", conf.User, base.DecryptAes(conf.Password, settedKey, settedIv), connectType, conf.Host, conf.DB))
 	if err != nil {
-		logError(err, 0)
+		info := fmt.Sprintf("%s:%s***@%s(%s)/%s", conf.User, conf.Password[0:5], connectType, conf.Host, conf.DB)
+		logError(err, &info, nil)
 		return &DB{conn: nil, Error: err}
 	}
 	db := new(DB)
@@ -121,559 +98,56 @@ func GetDB(name string) *DB {
 	return db
 }
 
-func (this *DB) Destroy() error {
-	if this.conn == nil {
-		return fmt.Errorf("Operat on a bad connection")
+func (db *DB) Destroy() error {
+	if db.conn == nil {
+		return errors.New("operate on a bad connection")
 	}
-	err := this.conn.Close()
-	logError(err, 0)
+	err := db.conn.Close()
+	logError(err, nil, nil)
 	return err
 }
 
-func (this *DB) GetOriginDB() *sql.DB {
-	if this.conn == nil {
+func (db *DB) GetOriginDB() *sql.DB {
+	if db.conn == nil {
 		return nil
 	}
-	return this.conn
+	return db.conn
 }
 
-func (this *Tx) Commit() error {
-	if this.conn == nil {
-		return fmt.Errorf("Operat on a bad connection")
-	}
-	err := this.conn.Commit()
-	logError(err, 0)
-	return err
+func (db *DB) Prepare(requestSql string) *Stmt {
+	return basePrepare(db.conn, nil, requestSql)
 }
 
-func (this *Tx) Rollback() error {
-	if this.conn == nil {
-		return fmt.Errorf("Operat on a bad connection")
+func (db *DB) Begin() *Tx {
+	if db.conn == nil {
+		return &Tx{Error: errors.New("operate on a bad connection")}
 	}
-	err := this.conn.Rollback()
-	logError(err, 0)
-	return err
-}
-
-func (this *Stmt) Exec(args ...interface{}) *ExecResult {
-	if this.conn == nil {
-		return &ExecResult{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-	r, err := this.conn.Exec(args...)
+	sqlTx, err := db.conn.Begin()
 	if err != nil {
-		logError(err, 0)
-		return &ExecResult{Error: err}
-	}
-	return &ExecResult{result: r}
-}
-
-func (this *Stmt) Close() error {
-	if this.conn == nil {
-		return fmt.Errorf("Operat on a bad connection")
-	}
-	err := this.conn.Close()
-	logError(err, 0)
-	return err
-}
-
-func (this *DB) Prepare(requestSql string) *Stmt {
-	return basePrepare(this.conn, nil, requestSql)
-}
-func (this *Tx) Prepare(requestSql string) *Stmt {
-	return basePrepare(nil, this.conn, requestSql)
-}
-func basePrepare(db *sql.DB, tx *sql.Tx, requestSql string) *Stmt {
-	var sqlStmt *sql.Stmt
-	var err error
-	if tx != nil {
-		sqlStmt, err = tx.Prepare(requestSql)
-	} else if db != nil {
-		sqlStmt, err = db.Prepare(requestSql)
-	} else {
-		return &Stmt{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-	if err != nil {
-		logError(err, 1)
-		return &Stmt{Error: err}
-	}
-	return &Stmt{conn: sqlStmt}
-}
-
-func (this *DB) Begin() *Tx {
-	if this.conn == nil {
-		return &Tx{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-	sqlTx, err := this.conn.Begin()
-	if err != nil {
-		logError(err, 1)
+		logError(err, nil, nil)
 		return &Tx{Error: nil}
 	}
 	return &Tx{conn: sqlTx}
 }
 
-func (this *DB) Exec(requestSql string, args ...interface{}) *ExecResult {
-	return baseExec(this.conn, nil, requestSql, args...)
-}
-func (this *Tx) Exec(requestSql string, args ...interface{}) *ExecResult {
-	return baseExec(nil, this.conn, requestSql, args...)
-}
-func baseExec(db *sql.DB, tx *sql.Tx, requestSql string, args ...interface{}) *ExecResult {
-	var r sql.Result
-	var err error
-	if tx != nil {
-		r, err = tx.Exec(requestSql, args...)
-	} else if db != nil {
-		r, err = db.Exec(requestSql, args...)
-	} else {
-		return &ExecResult{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-
-	if err != nil {
-		logError(err, 1)
-		return &ExecResult{Error: err}
-	}
-	return &ExecResult{result: r}
+func (db *DB) Exec(requestSql string, args ...interface{}) *ExecResult {
+	return baseExec(db.conn, nil, requestSql, args...)
 }
 
-func (this *DB) Query(requestSql string, args ...interface{}) *QueryResult {
-	return baseQuery(this.conn, nil, requestSql, args...)
-}
-func (this *Tx) Query(requestSql string, args ...interface{}) *QueryResult {
-	return baseQuery(nil, this.conn, requestSql, args...)
-}
-func baseQuery(db *sql.DB, tx *sql.Tx, requestSql string, args ...interface{}) *QueryResult {
-	var rows *sql.Rows
-	var err error
-	if tx != nil {
-		rows, err = tx.Query(requestSql, args...)
-	} else if db != nil {
-		rows, err = db.Query(requestSql, args...)
-	} else {
-		return &QueryResult{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-
-	if err != nil {
-		logError(err, 1)
-		return &QueryResult{Error: err}
-	}
-	return &QueryResult{rows: rows}
+func (db *DB) Query(requestSql string, args ...interface{}) *QueryResult {
+	return baseQuery(db.conn, nil, requestSql, args...)
 }
 
-func (this *DB) Insert(table string, data interface{}) *ExecResult {
-	return baseInsert(this.conn, nil, table, data, false)
+func (db *DB) Insert(table string, data interface{}) *ExecResult {
+	requestSql, values := makeInsertSql(table, data, false)
+	return baseExec(db.conn, nil, requestSql, values...)
 }
-func (this *Tx) Insert(table string, data interface{}) *ExecResult {
-	return baseInsert(nil, this.conn, table, data, false)
-}
-func (this *DB) Replace(table string, data interface{}) *ExecResult {
-	return baseInsert(this.conn, nil, table, data, true)
-}
-func (this *Tx) Replace(table string, data interface{}) *ExecResult {
-	return baseInsert(nil, this.conn, table, data, true)
-}
-func baseInsert(db *sql.DB, tx *sql.Tx, table string, data interface{}, useReplace bool) *ExecResult {
-	keys, vars, values := makeKeysVarsValues(data)
-	var operation string
-	if useReplace {
-		operation = "replace"
-	} else {
-		operation = "insert"
-	}
-	requestSql := fmt.Sprintf("%s into `%s` (`%s`) values (%s)", operation, table, strings.Join(keys, "`,`"), strings.Join(vars, ","))
-
-	var result sql.Result
-	var err error
-	if tx != nil {
-		result, err = tx.Exec(requestSql, values...)
-	} else if db != nil {
-		result, err = db.Exec(requestSql, values...)
-	} else {
-		return &ExecResult{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-
-	if err != nil {
-		logError(err, 1)
-		return &ExecResult{Error: err}
-	}
-	return &ExecResult{result: result}
+func (db *DB) Replace(table string, data interface{}) *ExecResult {
+	requestSql, values := makeInsertSql(table, data, true)
+	return baseExec(db.conn, nil, requestSql, values...)
 }
 
-func (this *DB) Update(table string, data interface{}, wheres string, args ...interface{}) *ExecResult {
-	return baseUpdate(this.conn, nil, table, data, wheres, args...)
-}
-func (this *Tx) Update(table string, data interface{}, wheres string, args ...interface{}) *ExecResult {
-	return baseUpdate(nil, this.conn, table, data, wheres, args...)
-}
-func baseUpdate(db *sql.DB, tx *sql.Tx, table string, data interface{}, wheres string, args ...interface{}) *ExecResult {
-	keys, vars, values := makeKeysVarsValues(data)
-	for i, k := range keys {
-		keys[i] = fmt.Sprintf("`%s`=%s", k, vars[i])
-	}
-	for _, v := range args {
-		values = append(values, v)
-	}
-	requestSql := fmt.Sprintf("update `%s` set %s where %s", table, strings.Join(keys, ","), wheres)
-
-	var result sql.Result
-	var err error
-	if tx != nil {
-		result, err = tx.Exec(requestSql, values...)
-	} else if db != nil {
-		result, err = db.Exec(requestSql, values...)
-	} else {
-		return &ExecResult{Error: fmt.Errorf("Operat on a bad connection")}
-	}
-	if err != nil {
-		logError(err, 1)
-		return &ExecResult{Error: err}
-	}
-	return &ExecResult{result: result}
-}
-
-func (this *ExecResult) Changes() int64 {
-	if this.result == nil {
-		return 0
-	}
-	numChanges, err := this.result.RowsAffected()
-	if err != nil {
-		logError(err, 1)
-		return 0
-	}
-	return numChanges
-}
-
-func (this *ExecResult) Id() int64 {
-	if this.result == nil {
-		return 0
-	}
-	insertId, err := this.result.LastInsertId()
-	if err != nil {
-		logError(err, 1)
-		return 0
-	}
-	return insertId
-}
-
-func (this *QueryResult) To(result interface{}) error {
-	if this.rows == nil {
-		return fmt.Errorf("Operat on a bad query")
-	}
-	return makeResults(result, this.rows)
-}
-
-func (this *QueryResult) MapResults() []map[string]interface{} {
-	result := make([]map[string]interface{}, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) SliceResults() [][]interface{} {
-	result := make([][]interface{}, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) StringMapResults() []map[string]string {
-	result := make([]map[string]string, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) StringSliceResults() [][]string {
-	result := make([][]string, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) MapOnR1() map[string]interface{} {
-	result := make(map[string]interface{})
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) SliceOnR1() []interface{} {
-	result := make([]interface{}, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) IntsOnC1() []int64 {
-	result := make([]int64, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) StringsOnC1() []string {
-	result := make([]string, 0)
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) IntOnR1C1() int64 {
-	var result int64 = 0
-	makeResults(&result, this.rows)
-	return result
-}
-
-func (this *QueryResult) StringOnR1C1() string {
-	result := ""
-	makeResults(&result, this.rows)
-	return result
-}
-
-func makeKeysVarsValues(data interface{}) ([]string, []string, []interface{}) {
-	keys := make([]string, 0)
-	vars := make([]string, 0)
-	values := make([]interface{}, 0)
-
-	dataType := reflect.TypeOf(data)
-	dataValue := reflect.ValueOf(data)
-	if dataType.Kind() == reflect.Ptr {
-		dataType = dataType.Elem()
-		dataValue = dataValue.Elem()
-	}
-
-	if dataType.Kind() == reflect.Struct {
-		// 按结构处理数据
-		for i := 0; i < dataType.NumField(); i++ {
-			v := dataValue.Field(i)
-			if v.Kind() == reflect.Interface {
-				v = v.Elem()
-			}
-			keys = append(keys, dataType.Field(i).Name)
-			if v.Kind() == reflect.String && []byte(v.String())[0] == ':' {
-				vars = append(vars, string([]byte(v.String())[1:]))
-			} else {
-				vars = append(vars, "?")
-				values = append(values, v.Interface())
-			}
-		}
-	} else if dataType.Kind() == reflect.Map {
-		// 按Map处理数据
-		for _, k := range dataValue.MapKeys() {
-			v := dataValue.MapIndex(k)
-			if v.Kind() == reflect.Interface {
-				v = v.Elem()
-			}
-			keys = append(keys, k.String())
-			if v.Kind() == reflect.String && v.Len() > 0 && []byte(v.String())[0] == ':' {
-				vars = append(vars, string([]byte(v.String())[1:]))
-			} else {
-				vars = append(vars, "?")
-				values = append(values, v.Interface())
-			}
-		}
-	}
-
-	return keys, vars, values
-}
-
-func makeResults(results interface{}, rows *sql.Rows) error {
-	rowType := reflect.TypeOf(results)
-	resultsValue := reflect.ValueOf(results)
-	if rowType.Kind() != reflect.Ptr {
-		err := fmt.Errorf("results must be a pointer")
-		logError(err, 0)
-		return err
-	}
-	rowType = rowType.Elem()
-	resultsValue = resultsValue.Elem()
-
-	colTypes, err := rows.ColumnTypes()
-	if err != nil {
-		logError(err, 0)
-		return err
-	}
-
-	colNum := len(colTypes)
-	if rowType.Kind() == reflect.Slice {
-		// 处理数组类型，非数组类型表示只取一行数据
-		rowType = rowType.Elem()
-	}
-
-	scanValues := make([]interface{}, colNum)
-	if rowType.Kind() == reflect.Struct {
-		// 按结构处理数据
-		for colIndex, col := range colTypes {
-			publicColName := makePublicVarName(col.Name())
-			field, found := rowType.FieldByName(publicColName)
-			if found {
-				if field.Type.Kind() == reflect.Interface {
-					scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
-				} else {
-					scanValues[colIndex] = makeValue(field.Type)
-				}
-			} else {
-				scanValues[colIndex] = makeValue(nil)
-			}
-		}
-	} else if rowType.Kind() == reflect.Map {
-		// 按Map处理数据
-		for colIndex := range colTypes {
-			if rowType.Elem().Kind() == reflect.Interface {
-				scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
-			} else {
-				scanValues[colIndex] = makeValue(rowType.Elem())
-			}
-		}
-	} else if rowType.Kind() == reflect.Slice {
-		// 按Map处理数据
-		for colIndex := range colTypes {
-			if rowType.Elem().Kind() == reflect.Interface {
-				scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
-			} else {
-				scanValues[colIndex] = makeValue(rowType.Elem())
-			}
-		}
-	} else {
-		// 只返回一列结果
-		if rowType.Kind() == reflect.Interface {
-			scanValues[0] = makeValue(colTypes[0].ScanType())
-		} else {
-			scanValues[0] = makeValue(rowType)
-		}
-		for colIndex := 1; colIndex < colNum; colIndex++ {
-			scanValues[colIndex] = makeValue(nil)
-		}
-	}
-
-	var data reflect.Value
-	for rows.Next() {
-
-		err = rows.Scan(scanValues...)
-		if err != nil {
-			logError(err, 0)
-			return err
-		}
-		if rowType.Kind() == reflect.Struct {
-			data = reflect.New(rowType).Elem()
-			for colIndex, col := range colTypes {
-				publicColName := makePublicVarName(col.Name())
-				_, found := rowType.FieldByName(publicColName)
-				if found {
-					valuePtr := reflect.ValueOf(scanValues[colIndex]).Elem()
-					if !valuePtr.IsNil() {
-						data.FieldByName(publicColName).Set(valuePtr.Elem())
-					}
-				}
-			}
-		} else if rowType.Kind() == reflect.Map {
-			// 结果放入Map
-			data = reflect.MakeMap(rowType)
-			for colIndex, col := range colTypes {
-				valuePtr := reflect.ValueOf(scanValues[colIndex]).Elem()
-				if !valuePtr.IsNil() {
-					data.SetMapIndex(reflect.ValueOf(col.Name()), valuePtr.Elem())
-				}
-			}
-		} else if rowType.Kind() == reflect.Slice {
-			// 结果放入Slice
-			data = reflect.MakeSlice(rowType, colNum, colNum)
-			for colIndex := range colTypes {
-				valuePtr := reflect.ValueOf(scanValues[colIndex]).Elem()
-				if !valuePtr.IsNil() {
-					data.Index(colIndex).Set(valuePtr.Elem())
-				}
-			}
-		} else {
-			// 只返回一列结果
-			valuePtr := reflect.ValueOf(scanValues[0]).Elem()
-			if !valuePtr.IsNil() {
-				data = valuePtr.Elem()
-			}
-		}
-
-		if resultsValue.Kind() == reflect.Slice {
-			resultsValue = reflect.Append(resultsValue, data)
-		} else {
-			resultsValue = data
-			break
-		}
-	}
-
-	reflect.ValueOf(results).Elem().Set(resultsValue)
-	//if this.conn.Stats().OpenConnections > 1 {
-	//	fmt.Println("conns:", this.conn.Stats().OpenConnections)
-	//}
-	return nil
-}
-
-func makePublicVarName(name string) string {
-	colNameBytes := []byte(name)
-	if colNameBytes[0] >= 97 {
-		colNameBytes[0] -= 32
-		return string(colNameBytes)
-	} else {
-		return name
-	}
-}
-
-func makeValue(t reflect.Type) interface{} {
-	if t == nil {
-		return new(*string)
-	}
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-
-	switch t.Kind() {
-	case reflect.Int:
-		return new(*int)
-	case reflect.Int8:
-		return new(*int8)
-	case reflect.Int16:
-		return new(*int16)
-	case reflect.Int32:
-		return new(*int32)
-	case reflect.Int64:
-		return new(*int64)
-	case reflect.Uint:
-		return new(*uint)
-	case reflect.Uint8:
-		return new(*uint8)
-	case reflect.Uint16:
-		return new(*uint16)
-	case reflect.Uint32:
-		return new(*uint32)
-	case reflect.Uint64:
-		return new(*uint64)
-	case reflect.Float32:
-		return new(*float32)
-	case reflect.Float64:
-		return new(*float64)
-	case reflect.Bool:
-		return new(*bool)
-	case reflect.String:
-		return new(*string)
-	}
-
-	return new(*string)
-	//if t.Kind() == reflect.Slice && t.Elem().Kind() == reflect.Uint8{
-	//	return new(string)
-	//}
-}
-
-func logError(err error, skips int) {
-	if enabledLogs && err != nil {
-		traces := make([]interface{}, 2)
-		traces[0] = "DB	"
-		traces[1] = err.Error()
-		i := 1
-		for {
-			_, file, line, ok := runtime.Caller(skips + i)
-			i++
-			if !ok {
-				break
-			}
-			if strings.Contains(file, "/go/src/") {
-				continue
-			}
-			pos := strings.Index(file, "/ssgo/db/")
-			if pos != -1 {
-				file = file[pos+9:]
-			}
-			traces = append(traces, fmt.Sprintf("	%s:%d", file, line))
-		}
-		log.Print(traces...)
-	}
+func (db *DB) Update(table string, data interface{}, wheres string, args ...interface{}) *ExecResult {
+	requestSql, values := makeUpdateSql(table, data, wheres, args...)
+	return baseExec(db.conn, nil, requestSql, values...)
 }
---
+++ Result.go
@@ -0,0 +1,298 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type QueryResult struct {
+	rows  *sql.Rows
+	Sql   *string
+	Args  []interface{}
+	Error error
+}
+
+type ExecResult struct {
+	result sql.Result
+	Sql    *string
+	Args   []interface{}
+	Error  error
+}
+
+func (r *ExecResult) Changes() int64 {
+	if r.result == nil {
+		return 0
+	}
+	numChanges, err := r.result.RowsAffected()
+	if err != nil {
+		logError(err, r.Sql, r.Args)
+		return 0
+	}
+	return numChanges
+}
+
+func (r *ExecResult) Id() int64 {
+	if r.result == nil {
+		return 0
+	}
+	insertId, err := r.result.LastInsertId()
+	if err != nil {
+		logError(err, r.Sql, r.Args)
+		return 0
+	}
+	return insertId
+}
+
+func (r *QueryResult) To(result interface{}) error {
+	if r.rows == nil {
+		return errors.New("operate on a bad query")
+	}
+	return r.makeResults(result, r.rows)
+}
+
+func (r *QueryResult) MapResults() []map[string]interface{} {
+	result := make([]map[string]interface{}, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) SliceResults() [][]interface{} {
+	result := make([][]interface{}, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) StringMapResults() []map[string]string {
+	result := make([]map[string]string, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) StringSliceResults() [][]string {
+	result := make([][]string, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) MapOnR1() map[string]interface{} {
+	result := make(map[string]interface{})
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) SliceOnR1() []interface{} {
+	result := make([]interface{}, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) IntsOnC1() []int64 {
+	result := make([]int64, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) StringsOnC1() []string {
+	result := make([]string, 0)
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) IntOnR1C1() int64 {
+	var result int64 = 0
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) StringOnR1C1() string {
+	result := ""
+	r.makeResults(&result, r.rows)
+	return result
+}
+
+func (r *QueryResult) makeResults(results interface{}, rows *sql.Rows) error {
+	rowType := reflect.TypeOf(results)
+	resultsValue := reflect.ValueOf(results)
+	if rowType.Kind() != reflect.Ptr {
+		err := fmt.Errorf("results must be a pointer")
+		logError(err, r.Sql, r.Args)
+		return err
+	}
+	rowType = rowType.Elem()
+	resultsValue = resultsValue.Elem()
+
+	colTypes, err := rows.ColumnTypes()
+	if err != nil {
+		logError(err, r.Sql, r.Args)
+		return err
+	}
+
+	colNum := len(colTypes)
+	if rowType.Kind() == reflect.Slice {
+		// 处理数组类型，非数组类型表示只取一行数据
+		rowType = rowType.Elem()
+	}
+
+	scanValues := make([]interface{}, colNum)
+	if rowType.Kind() == reflect.Struct {
+		// 按结构处理数据
+		for colIndex, col := range colTypes {
+			publicColName := makePublicVarName(col.Name())
+			field, found := rowType.FieldByName(publicColName)
+			if found {
+				if field.Type.Kind() == reflect.Interface {
+					scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
+				} else {
+					scanValues[colIndex] = makeValue(field.Type)
+				}
+			} else {
+				scanValues[colIndex] = makeValue(nil)
+			}
+		}
+	} else if rowType.Kind() == reflect.Map {
+		// 按Map处理数据
+		for colIndex := range colTypes {
+			if rowType.Elem().Kind() == reflect.Interface {
+				scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
+			} else {
+				scanValues[colIndex] = makeValue(rowType.Elem())
+			}
+		}
+	} else if rowType.Kind() == reflect.Slice {
+		// 按Map处理数据
+		for colIndex := range colTypes {
+			if rowType.Elem().Kind() == reflect.Interface {
+				scanValues[colIndex] = makeValue(colTypes[colIndex].ScanType())
+			} else {
+				scanValues[colIndex] = makeValue(rowType.Elem())
+			}
+		}
+	} else {
+		// 只返回一列结果
+		if rowType.Kind() == reflect.Interface {
+			scanValues[0] = makeValue(colTypes[0].ScanType())
+		} else {
+			scanValues[0] = makeValue(rowType)
+		}
+		for colIndex := 1; colIndex < colNum; colIndex++ {
+			scanValues[colIndex] = makeValue(nil)
+		}
+	}
+
+	var data reflect.Value
+	for rows.Next() {
+
+		err = rows.Scan(scanValues...)
+		if err != nil {
+			logError(err, r.Sql, r.Args)
+			return err
+		}
+		if rowType.Kind() == reflect.Struct {
+			data = reflect.New(rowType).Elem()
+			for colIndex, col := range colTypes {
+				publicColName := makePublicVarName(col.Name())
+				_, found := rowType.FieldByName(publicColName)
+				if found {
+					valuePtr := reflect.ValueOf(scanValues[colIndex]).Elem()
+					if !valuePtr.IsNil() {
+						data.FieldByName(publicColName).Set(valuePtr.Elem())
+					}
+				}
+			}
+		} else if rowType.Kind() == reflect.Map {
+			// 结果放入Map
+			data = reflect.MakeMap(rowType)
+			for colIndex, col := range colTypes {
+				valuePtr := reflect.ValueOf(scanValues[colIndex]).Elem()
+				if !valuePtr.IsNil() {
+					data.SetMapIndex(reflect.ValueOf(col.Name()), valuePtr.Elem())
+				}
+			}
+		} else if rowType.Kind() == reflect.Slice {
+			// 结果放入Slice
+			data = reflect.MakeSlice(rowType, colNum, colNum)
+			for colIndex := range colTypes {
+				valuePtr := reflect.ValueOf(scanValues[colIndex]).Elem()
+				if !valuePtr.IsNil() {
+					data.Index(colIndex).Set(valuePtr.Elem())
+				}
+			}
+		} else {
+			// 只返回一列结果
+			valuePtr := reflect.ValueOf(scanValues[0]).Elem()
+			if !valuePtr.IsNil() {
+				data = valuePtr.Elem()
+			}
+		}
+
+		if resultsValue.Kind() == reflect.Slice {
+			resultsValue = reflect.Append(resultsValue, data)
+		} else {
+			resultsValue = data
+			break
+		}
+	}
+
+	reflect.ValueOf(results).Elem().Set(resultsValue)
+	//if r.conn.Stats().OpenConnections > 1 {
+	//	fmt.Println("conns:", r.conn.Stats().OpenConnections)
+	//}
+	return nil
+}
+
+func makePublicVarName(name string) string {
+	colNameBytes := []byte(name)
+	if colNameBytes[0] >= 97 {
+		colNameBytes[0] -= 32
+		return string(colNameBytes)
+	} else {
+		return name
+	}
+}
+
+func makeValue(t reflect.Type) interface{} {
+	if t == nil {
+		return new(*string)
+	}
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	switch t.Kind() {
+	case reflect.Int:
+		return new(*int)
+	case reflect.Int8:
+		return new(*int8)
+	case reflect.Int16:
+		return new(*int16)
+	case reflect.Int32:
+		return new(*int32)
+	case reflect.Int64:
+		return new(*int64)
+	case reflect.Uint:
+		return new(*uint)
+	case reflect.Uint8:
+		return new(*uint8)
+	case reflect.Uint16:
+		return new(*uint16)
+	case reflect.Uint32:
+		return new(*uint32)
+	case reflect.Uint64:
+		return new(*uint64)
+	case reflect.Float32:
+		return new(*float32)
+	case reflect.Float64:
+		return new(*float64)
+	case reflect.Bool:
+		return new(*bool)
+	case reflect.String:
+		return new(*string)
+	}
+
+	return new(*string)
+	//if t.Kind() == reflect.Slice && t.Elem().Kind() == reflect.Uint8{
+	//	return new(string)
+	//}
+}
