f569867270686a49487f40a5f3769d6a983aec80
Star <star3s@126.com>
2018-07-04 21:53:30 +0800

    first

---
+++ Config.go
@@ -0,0 +1,22 @@
+package discover
+
+var config Config
+
+type Config struct {
+	Registry          string               // "127.0.0.1:6379:15"、"127.0.0.1:6379:15:password"
+	RegistryPrefix    string               // ""
+	RegistryCalls     string               // same with RegistryHost
+	App               string               // register to a app service
+	Weight            uint                 // 1
+	Calls             map[string]*CallInfo // defines which apps will call
+	CallRetryTimes    uint8                // 10
+	XForwardedForName string               // X-Forwarded-For
+	XRealIpName       string               // X-Real-Ip
+	CallTimeout       int                  // 10000
+}
+
+type CallInfo struct {
+	Headers     map[string]string
+	Timeout     int
+	HttpVersion int
+}
